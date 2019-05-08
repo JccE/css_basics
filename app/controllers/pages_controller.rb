2822e9dc2eaaa9c6f433d5c8bb7bcf6bca531ef2
jcce <toshajo99@gmail.com>
2019-05-07 23:30:20 -0500

    end of basics

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,49 @@
+class PagesController < ApplicationController
+  def main
+  end
+
+  def example_card
+  end
+
+  def box_model
+  end
+
+  def pseudo_elements
+  end
+
+  def pseudo_elements_2
+  end
+
+  def pseudo_elements_3
+  end
+
+  def measurement_units
+  end
+
+  def measurement_units_2
+  end
+
+  def positions
+  end
+
+  def overflow_float
+  end
+
+  def backgrounds
+  end
+
+  def gradients
+  end
+
+  def shadows
+  end
+
+  def transitions
+  end
+
+  def transforms
+  end
+
+  def animations
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "pages#example_card"
+
+  get 'box_model', to: 'pages#box_model'
+
+  get 'pseudo_elements', to: 'pages#pseudo_elements'
+
+  get 'pseudo_elements_2', to: 'pages#pseudo_elements_2'
+
+  get 'pseudo_elements_3', to: 'pages#pseudo_elements_3'
+
+  get 'measurement_units', to: 'pages#measurement_units'
+
+  get 'measurement_units_2', to: 'pages#measurement_units_2'
+
+  get 'positions', to: 'pages#positions'
+
+  get 'overflow_float', to: 'pages#overflow_float'
+
+  get 'backgrounds', to: 'pages#backgrounds'
+
+  get 'gradients', to: 'pages#gradients'
+
+  get 'shadows', to: 'pages#shadows'
+
+  get 'transitions', to: 'pages#transitions'
+
+  get 'transforms', to: 'pages#transforms'
+
+  get 'animations', to: 'pages#animations'
+
 end
